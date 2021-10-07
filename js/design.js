981f764080001856078dd41c07d66b48c87220ff
xterikagh <xterikagh@gmail.com>
2021-10-07 12:05:08 +0000

    new

--- js/design.js
+++ js/design.js
@@ -4,6 +4,15 @@
          rootHeight: '100%',
          rootWidth: '100%',
          backgroundColor: '#4F2651',
+        /* bubbleBackground: '#fff',
+        bubbleBorder: 'solid 1px #444',
+        bubbleBorderRadius: 20,
+        bubbleFromUserBackground: '#222',
+        bubbleFromUserBorder: 'solid 1px #444',
+        bubbleFromUserBorderRadius: 20,
+        bubbleFromUserTextColor: 'White',
+        bubbleTextColor: 'black'
+        */
          
       });
 
@@ -11,16 +20,30 @@
     const avatarOptions = {
     botAvatarImage: 'https://www.graphic.com.gh/images/2019/may/02/first_atlantic.png',
     botAvatarInitials: 'OH',
-    userAvatarImage: 'https://icons-for-free.com/iconfiles/png/512/avatar+human+male+man+men+people+person+profile+user+users-1320196163635839021.png',
     userAvatarInitials: 'GH',
     hideUploadButton: true // Disable upload button
          };
 
-    window.WebChat.renderWebChat({
-      directLine: window.WebChat.createDirectLine({
-          token: 'Ng3Ci7HCDZE.-38t612x0jmvOFAT1CEzx0wesntMoLOg5L5MU-3zZ2Q'}),
+    
+    /*window.WebChat.renderWebChat({
+          
+        directLine: window.WebChat.createDirectLine({ token: '8zk_5f7eDYQ.0KsgURWPaFKdQEpHO--dOg77OvQdyRVVzbIRx04BqBY'}),
+          
           styleSet,
           styleOptions: avatarOptions, // Enable Avatar icon.
      
     }, document.getElementById('webchat'));
     
+
+    */
+
+    (async function () {
+           const res = await fetch('https://azurenewest.azurewebsites.net/directline/token', { method: 'POST' });
+          const { token } = await res.json();
+          window.WebChat.renderWebChat({
+             directLine: window.WebChat.createDirectLine({ token }),
+             styleSet,
+
+          }, document.getElementById('webchat'));
+         })();
+
